26827c5c83522f30620bace603b9879ab9a185f4
zzk0910 <460971792@qq.com>
2021-11-03 16:45:46 +0800

    修改chatbot page及相关文件

--- GUI/ChatbotPage.py
+++ GUI/ChatbotPage.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
-from tkinter.colorchooser import askcolor
 from tkinter import ttk
+from ERWord2VecDL.serving.predict import run_api
+from Generation.generation import load_generation_model, gen
+import random
+import PIL
 from PIL import Image
 from PIL import ImageTk
-# import gpt_2_simple as gpt2
+import json
+
 
 class ChatbotPage(tk.Frame):
     def __init__(self, parent, root):
@@ -13,111 +17,161 @@ class ChatbotPage(tk.Frame):
         self.config = {
             "window": "900x600",
             "message": {
-                "font": ["calibri",14],
+                "font": ["calibri", 14],
                 "font_color": "balck",
                 "bd": 4,
                 "bg": "white"
             },
             "question": {
-                "font": ["calibri",14],
+                "font": ["calibri", 14],
                 "font_color": "balck",
                 "bd": 4,
                 "bg": "white"
             },
             "button": {
-                "font": ["calibri",14],
+                "font": ["calibri", 14],
                 "font_color": "black",
                 "bd": 4,
                 "bg": "white"
-            }
+            },
+            "generation_nodel_dir": "../Generation/checkpoint",
+            "emotion_classification_dir": ""
+
         }
-        self.rowconfigure(index=0, weight=6)
-        self.rowconfigure(index=1, weight=1)
+
+        self.sess2 = load_generation_model()    # Start gpt-2 model
 
         chat_frame = self.create_chat_frame()
-        chat_frame.grid(column=0, row=0)
+        chat_frame.grid(row=0, column=0)
 
         button_frame = self.create_button_frame()
-        button_frame.grid(column=0, row=1)
+        button_frame.grid(row=1, column=0)
 
-        # @staticmethod
-        # def activate_model(checkpoint_dir):
-        #     sess = gpt2.start_tf_sess()
-        #     gpt2.load_gpt2(sess, run_name="run2", checkpoint_dir=checkpoint_dir, reuse=True)
-        #
-        #     return sess
-        #
-        # self.chat_session = activate_model("../Generation/checkpoint")
-        #
-        # def about():
-        #     messagebox.showinfo('关于聊天机器人', '开发者：***\n开发日期：**** 年 ** 月')
-        # def helper():
-        #     messagebox.showinfo('关于聊天机器人',
-        #                         '使用说明：\n1.在底下的发送框输入信息；\n2.点击“发送”按钮发送消息；\n3.聊天机器人回复。\n\n注意事项：\n1.程序不能回车发送\n2.使用时请连接网络\n3.回复个别问题可能出现乱码，请见谅')
 
     def create_chat_frame(self):
-        chat_frame = ttk.Frame(self)
+        chat_frame = ttk.Frame(self, width=500, height=200)
         # chat_frame['padding'] = 5
+        chat_frame.rowconfigure(0, weight=1)
+        chat_frame.rowconfigure(1, weight=1)
         chat_frame.columnconfigure(0, weight=4)
         chat_frame.columnconfigure(1, weight=1)
-        chat_frame.columnconfigure(2, weight=2)
-        chat_frame.rowconfigure(0, weight=3)
-        chat_frame.rowconfigure(1, weight=2)
-        chat_frame.rowconfigure(2, weight=1)
+        chat_frame.columnconfigure(1, weight=1)
 
-        textShow = scrolledtext.ScrolledText(chat_frame, bd=self.config["message"]["bd"], font=self.config["message"]["font"])
+        # Create text chat interface
+        textShow = tk.Text(chat_frame, height=35, width=70,
+                           bd=self.config["message"]["bd"],
+                           font=self.config["message"]["font"])
+        textShow.grid(row=0, column=0, columnspan=2, sticky="nesw")
+
+        textShow.tag_add("user", tk.END)
+        textShow.tag_add("littlegenesis", tk.END)
+        textShow.tag_add("input_sentence", tk.END)
+        textShow.tag_config("user", foreground="blue", font=self.config["message"]["font"])
+        textShow.tag_config("littlegenesis", foreground="green", font=self.config["message"]["font"])
+        textShow.tag_config("input_sentence", foreground="green", font=self.config["message"]["font"])
+
+        # create a scrollbar widget and set its command to the text widget
+        scrollbar = ttk.Scrollbar(chat_frame, orient='vertical', command=textShow.yview)
+        scrollbar.grid(row=0, column=2, sticky='nsw')
+        #  communicate back to the scrollbar
+        textShow['yscrollcommand'] = scrollbar.set
+
+        textInput = tk.Entry(
+            chat_frame,
+            bd=self.config["question"]["bd"],
+            font=self.config["question"]["font"])
+        textInput.grid(row=1, column=0, sticky="ew")
+
+        # Welcome notes
+        textShow.insert(tk.END, "Hi ! I'm little Genesis, nice to meet you! \n" +
+                               "Just share me anything you want to say. I'm excited to liten to your story! \n\n")
+        textShow.see(tk.END)
         textShow.config(state='disabled')
-        textShow.grid(column=0, row=0, rowspan=2, columnspan=2, sticky="nsew")
 
-        textInput = tk.Entry(chat_frame, bd=self.config["question"]["bd"], font=self.config["question"]["font"])
-        textInput.grid(column=0, row=2, sticky="nsew")
+        with open("../Generation/rules.json", 'r') as f:
+            responses = json.load(f)
+            fix_response = responses["daily"]
+            emotion_response = responses["emotions"]
+        emotion_threshold = {
+            1: ["sadness", 0.6],  # sad
+            2: ["fear", 0.6],  # fear
+            3: ["anger", 0.6],  # anger
+            4: ["joy", 0.6],  # joy
+            5: ["disgust", 0.6],  # disgust
+            6: ["surprise", 0.6]  # surprise
+        }
 
-        def chat():
-            return "good!"
+        def response_strategy(sess2, f_reponse, e_response, input_sentence, emotion_threshold):
+            if input_sentence in f_reponse:
+                answer = random.choice(f_reponse[input_sentence])
+            else:
+                # label, value = run_api(input_sentence)
+                label, value = 3, 0.3
+                if value >= emotion_threshold[label][1]:
+                    e_type = emotion_threshold[label][0]
+                    prefix = random.choice(e_response[e_type])
+                    answer = gen(sess2, prefix, include_prefix=True)
+                else:
+                    answer = gen(sess2, input_sentence, include_prefix=False)
+
+            return answer
 
         def send_message():
+            q = textInput.get()
+            # textInput.delete(0, tk.END)
+            if q.replace(" ", "") == "":
+                # messagebox.showwarning('Warning', "Empty Message！")
+                return
+
+            # 存储需要的东西到数据库 sentence就是q
+            """
+            
+            
+            
+            """
+
+            textShow.config(state='normal')
+            textShow.insert(tk.END, "User：\n", "user")
+            textShow.insert(tk.END, q, "user")
+            textShow.insert(tk.END, "\n\n", "user")
+            textShow.see(tk.END)
+            textShow.config(state='disabled')
+
+        def give_response():
             q = textInput.get()
             textInput.delete(0, tk.END)
             if q.replace(" ", "") == "":
                 messagebox.showwarning('Warning', "Empty Message！")
                 return
+
             textShow.config(state='normal')
-            textShow.insert(tk.END, "User：" + q + "\n")
-            textShow.insert(tk.END, "Little Genesis：" + chat() + "\n")
+            textShow.insert(tk.END, "Little Genesis：\n", "littlegenesis")
+            textShow.insert(tk.END,
+                            response_strategy(self.sess2, fix_response, emotion_response, q, emotion_threshold),
+                            "input_sentence")
+            textShow.insert(tk.END, "\n\n", "littlegenesis")
             textShow.see(tk.END)
             textShow.config(state='disabled')
 
-        def show_usr_info():
-            tk.Toplevel()
+
+        # def button_action():
+        # q = textInput.get()
+        # textInput.delete(0, tk.END)
+        # if q.replace(" ", "") == "":
+        #     messagebox.showwarning('Warning', "Empty Message！")
+        #     return
+        # send_message(q)
+        # give_response(q)
 
         send = ttk.Button(chat_frame,
                           text='SEND',
-                          command=send_message)
-        send.grid(column=1, row=2, sticky="nsew")
-
-        # load = Image.open("/Users/zzkzz/Pictures/截图/iShot2021-10-18 16.08.50.png")   # open image from path
-        # img = ImageTk.PhotoImage(load)                      # read opened image
-        # label1 = tk.Label(chat_frame, image=img)            # create a label to insert this image
-        # label1.grid(column=2, row=0, sticky="nsew")
-
-        usr_info = ttk.Button(chat_frame,
-                               text='Myinfo',
-                               command=show_usr_info)
-        usr_info.grid(column=2, row=1, rowspan=2, sticky="nsew")
+                          command=lambda: [send_message(), give_response()])
+        send.grid(row=1, column=1, sticky="ew")
 
         return chat_frame
 
-        # menu = Menu(root)
-        # root.config(menu=menu)
-        # bot = Menu(menu)
-        # bot.add_command(label='程序信息', command=about)
-        # bot.add_command(label='帮助', command=helper)
-        # menu.add_cascade(label='关于', menu=bot)
-
-        # root.mainloop()
-
     def create_button_frame(self):
-        button_frame = ttk.Frame(self)
+        button_frame = ttk.Frame(self, width=500, height=150)
         button_frame['padding'] = 5
         button_frame.columnconfigure(0, weight=1)
         button_frame.columnconfigure(1, weight=1)
@@ -128,25 +182,26 @@ class ChatbotPage(tk.Frame):
                                           style='Button_OFF.TButton',
                                           text="Questionnaire",
                                           command=lambda: self.root.show_page('QuestionnairePage'))
-        questionnaire_button.grid(column=0, row=0, sticky=tk.W, padx=2, pady=2)
+        questionnaire_button.grid(column=0, row=0, sticky='ew', padx=2, pady=2)
 
         # navigator button to Chatroom
         chatbot_button = ttk.Button(button_frame,
                                     style='Button_ON.TButton',
                                     text="Little Genesis",
                                     command=lambda: self.root.show_page('ChatbotPage'))
-        chatbot_button.grid(column=1, row=0, sticky=tk.W, padx=2, pady=2)
+        chatbot_button.grid(column=1, row=0, sticky='ew', padx=2, pady=2)
 
         # navigator button to Monitor
         monitor_button = ttk.Button(button_frame,
                                     style='Button_OFF.TButton',
                                     text="Long-term Monitoring",
                                     command=lambda: self.root.show_page('MonitorPage'))
-        monitor_button.grid(column=2, row=0, sticky=tk.W, padx=2, pady=2)
+        monitor_button.grid(column=2, row=0, sticky='ew', padx=2, pady=2)
 
         return button_frame
 
 
 
-if __name__ == "__main__":
-    ChatbotPage()
+#
+# if __name__ == "__main__":
+#     ChatbotPage()
---
+++ Generation/generation.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import json
+import time
+import random
+import gpt_2_simple as gpt2
+import tensorflow as tf
+
+import numpy as np
+#
+# print(platform.platform())
+# # -------------------------- 对传入的标签类进行处理 -----------------------------------------
+# # The format of classification results received: (label, value) e.g. ("sadness", 0.34)
+#
+# label, value = ("sadness", 0.34)
+#
+# CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+# RULE_DIR = os.path.join(CUR_DIR, 'rules.json')
+# with open(RULE_DIR, 'r') as f:
+#     rules = json.load(f)
+#     f.close()
+#
+# if label == "sadness" and value >= 0.30:
+#     prefix = random.choice(rules["sadness"])
+# elif label == "fear" and value >= 0.30:
+#     prefix = random.choice(rules["fear"])
+# else:
+#     prefix = "Good"
+
+# ------------------- 使用GPT-2模型进行语言生成 ---------------------------
+
+
+def load_generation_model():
+    checkpoint_dir = "../Generation/checkpoint"
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess, run_name="run2", checkpoint_dir=checkpoint_dir)
+
+    return sess
+
+
+def gen(sess, prefix, include_prefix):
+    checkpoint_dir = "../Generation/checkpoint"
+    if include_prefix:
+        answers = gpt2.generate(sess=sess,
+                                run_name="run2",
+                                checkpoint_dir=checkpoint_dir,
+                                length=30,
+                                temperature=0.7,
+                                seed=random.randint(1, 1000),
+                                prefix=prefix,
+                                nsamples=5,
+                                top_k=10,
+                                top_p=0.9,
+                                include_prefix=True,
+                                return_as_list=True,
+                                batch_size=5)
+    else:
+        answers = gpt2.generate(sess=sess,
+                                run_name="run2",
+                                checkpoint_dir=checkpoint_dir,
+                                length=30,
+                                temperature=0.7,
+                                seed=random.randint(1, 1000),
+                                prefix=prefix,
+                                nsamples=5,
+                                top_k=10,
+                                top_p=0.9,
+                                include_prefix=False,
+                                truncate='.\n',
+                                return_as_list=True,
+                                batch_size=5)
+
+    # print(answers[random.randint(0, 4)])
+
+    return answers[0]
+
+
+if __name__ == "__main__":
+    sess = load_generation_model()
+    # answer = gen(sess, "I'm good")
+    # print(answer)
